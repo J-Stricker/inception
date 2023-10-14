6b7df0fba73407b54ca465faa058e32741047b7b
fsariogl <fsariogl@fsariogl.fsariogl>
2023-10-14 13:44:25 -0400

    projet fonctionnel

--- inc/srcs/requirements/mariadb/conf/script.sh
+++ inc/srcs/requirements/mariadb/conf/script.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
-mysql -e "CREATE DATABASE IF NOT EXISTS ${DB_DATABASE};"
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PASSWORD}';"
-mysql -e "GRANT ALL PRIVILEGES ON {$DB_DATABASE}.* TO '${DB_USER}'@'%' IDENTIFIED BY '${DB_USER_PASSWORD}';"
-mysql -e "FLUSH PRIVILEGES;"
-mysqld --bind-address=0.0.0.0
+if [ ! -d "/var/lib/mysql/${DB_NAME}" ]
+then
+
+	service		mysql start
+	sleep 5
+	mysql -e 	"CREATE DATABASE ${DB_NAME};"
+	mysql -e	"CREATE USER '${DB_USER}'@'%' IDENTIFIED BY '${DB_USER_PWD}';"
+	mysql -e	"GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER}'@'%' IDENTIFIED BY '${DB_USER_PWD}' WITH GRANT OPTION;"
+	mysql -e	"FLUSH PRIVILEGES;"
+#	sed -i		"s/password =/password = ${DB_USER_PWD} #/" /etc/mysql/debian.cnf
+	mysql -e	"ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_USER_PWD}';"
+	service		mysql stop
+fi
+
+mysqld_safe
+
