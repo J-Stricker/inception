0349aee8e70d3af921b515608e9ef2aef243c703
fsariogl <fsariogl@fsariogl.fsariogl>
2023-10-17 11:41:35 -0400

    finish

--- inc/srcs/requirements/mariadb/conf/script.sh
+++ inc/srcs/requirements/mariadb/conf/script.sh
@@ -3,12 +3,10 @@ if [ ! -d "/var/lib/mysql/${DB_NAME}" ]
 then
 
 	service		mysql start
-#	sleep 5
 	mysql -e 	"CREATE DATABASE ${DB_NAME};"
 	mysql -e	"CREATE USER '${DB_USER}'@'%' IDENTIFIED BY '${DB_USER_PWD}';"
 	mysql -e	"GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER}'@'%' IDENTIFIED BY '${DB_USER_PWD}' WITH GRANT OPTION;"
 	mysql -e	"FLUSH PRIVILEGES;"
-#	sed -i		"s/password =/password = ${DB_USER_PWD} #/" /etc/mysql/debian.cnf
 	mysql -e	"ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_USER_PWD}';"
 	service		mysql stop
 fi
